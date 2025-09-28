79e44fc70e2ab28d04d74567c2453fdee66c0cd8
Michella <mkri3904@uni.sydney.edu.au>
2025-09-28 20:42:52 +1000

    Change message numbers and colours - change blacks to burgandy - change result message to numbered

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const tasks = [
-  "Flash Recall: pick a random topic and recall it without notes.",
-  "Code Skeleton Drill: write minimal syntax for a topic.",
-  "One Past Question: attempt one exam-style question.",
-  "Explain Out Loud: teach the concept in 1 minute.",
-  "Flashcard Round: review 5-10 cards.",
-  "Cheat Sheet Update: add/refine one example.",
-  "Compare & Correct: rewrite an example from memory.",
-  "Quickfire Questions: make and answer 3 mini questions.",
-  "Error Hunt: break and then fix a snippet.",
-  "Mini-Mock: 15 mins, 2-3 short exam-style questions."
+  "1. Flash Recall: pick a random topic and recall it without notes.",
+  "2. Code Skeleton Drill: write minimal syntax for a topic.",
+  "3. One Past Question: attempt one exam-style question.",
+  "4. Explain Out Loud: teach the concept in 1 minute.",
+  "5. Flashcard Round: review 5-10 cards.",
+  "6. Cheat Sheet Update: add/refine one example.",
+  "7.Compare & Correct: rewrite an example from memory.",
+  "8. Quickfire Questions: make and answer 3 mini questions.",
+  "9. Error Hunt: break and then fix a snippet.",
+  "10. Mini-Mock: 15 mins, 2-3 short exam-style questions."
 ];
 
 const wheel = document.getElementById('wheel');
@@ -23,6 +23,6 @@ function spin() {
   setTimeout(() => {
     const normalized = deg % 360;
     const index = Math.floor(((360 - normalized) % 360) / 36);
-    document.getElementById('result').textContent = "→ " + tasks[index];
+    document.getElementById('result').textContent =  tasks[index];
   }, 4200);
 }
